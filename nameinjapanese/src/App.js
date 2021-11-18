548c020ebee072ba79e0e6400afd60b8a36ee336
Kumirei <linusson.sixten@gmail.com>
2021-11-18 16:02:29 -0500

    finish?

--- nameinjapanese/src/App.js
+++ nameinjapanese/src/App.js
@@ -1,39 +1,246 @@
 import './App.css'
 import * as React from 'react'
+// Material UI
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import nameInJapanese from './yourname'
+import Card from '@mui/material/Card'
+import Box from '@mui/material/Box'
+import CardContent from '@mui/material/CardContent'
+import Toolbar from '@mui/material/Toolbar'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Paper from '@mui/material/Paper'
+import { DataGrid } from '@mui/x-data-grid'
+import Link from '@mui/material/Link'
+import Stack from '@mui/material/Stack'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+// import { ThemeProvider, createTheme } from '@mui/system'
+// My stuffs
+import getJapaneseNames from './yourname'
+import { Container } from '@mui/material'
+
+const default_data = [
+    { names: [{ name: 'Your', freq: 1, articles: [] }] },
+    { names: [{ name: 'Name', freq: 1, articles: [] }] },
+]
+
+const theme = createTheme({
+    palette: {
+        primary: { main: '#000', accent: 'hsl(300, 20%, 50%)', link: 'hsl(300, 60%, 40%)' },
+        secondary: { main: '#fff' },
+        background: {
+            main: '#999',
+            paper: '#999',
+            dark: '#888',
+            separators: '#666',
+        },
+    },
+})
 
 function App() {
-    const [name, setName] = React.useState('')
+    const [data, setData] = React.useState(default_data) // Data about names
+    const [selected, setSelected] = React.useState({ name: '', spelling: '' }) // Keep track of what data is selected for the table
 
-    const processAfterMs = 500
+    // Wait until user stops typing then initiate processing
+    const processInputAfterMs = 500
     let timeout = 0
     let lastInput = 0
-    function handleInputChange(event, b, c) {
-        if ((event.timeStamp - lastInput) / 1000 < processAfterMs / 100) {
+    function handleInputChange(event) {
+        if ((event.timeStamp - lastInput) / 1000 < processInputAfterMs / 100) {
             clearTimeout(timeout)
             timeout = setTimeout(async () => {
-                console.log('timeout', event.target.value)
-                const japaneseName = await nameInJapanese(event.target.value)
-                setName(japaneseName)
-            }, processAfterMs)
+                const dataPromise = getJapaneseNames(event.target.value)
+                setData(
+                    event.target.value
+                        .split(/\s/)
+                        .map((n) => ({ names: [{ name: 'Loading...', freq: 0, articles: [] }] })),
+                ) // Set data to show 'Loading...' on the cards while fetching
+                const data = await dataPromise
+                setData(data)
+                setSelected({ name: 0, spelling: 0 })
+            }, processInputAfterMs)
         }
         lastInput = event.timeStamp
     }
 
+    // Upadate selected variable with new data
+    const handleSelectedChange = (update) => {
+        setSelected({ ...selected, ...update })
+    }
+
+    // Column template for table
+    const columns = [
+        { field: 'id', headerName: '#', flex: 0, width: 60 },
+        { field: 'ja', headerName: 'Japanese', flex: 1 },
+        { field: 'en', headerName: 'English', flex: 1 },
+        {
+            field: 'link',
+            headerName: 'Wikipedia',
+            flex: 0,
+            sortable: false,
+            renderCell: (params) => (
+                <Link
+                    href={`https://en.wikipedia.org/?curid=${params.value}`}
+                    sx={{ color: 'primary.link', textDecorationColor: 'hsl(300, 60%, 40%)' }}
+                >
+                    Link
+                </Link>
+            ),
+        },
+    ]
+
+    // The data for the table
+    const rows =
+        data[selected.name || 0].names[selected.spelling || 0]?.articles.map((r, i) => ({
+            ...r,
+            id: i + 1,
+            link: r.id,
+        })) ?? {}
+
     return (
-        <div className='App'>
-            <header>
-                <Typography variant='h6' gutterBottom component='h1'>
-                    Your Name In Japanese
-                </Typography>
-            </header>
-            <TextField fullwidth onChange={handleInputChange} id='outlined-basic' label='Outlined' variant='outlined' />
-            <Typography variant='p' gutterBottom component='p'>
-                Your name is {name}
-            </Typography>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Container className='App' sx={{ textAlign: 'center' }}>
+                <Box component='header' sx={{ pt: '1em', color: 'secondary.main' }}>
+                    <Typography variant='h3' gutterBottom component='h1'>
+                        Your Name In Japanese
+                    </Typography>
+                </Box>
+                <Container component='section' style={{ maxWidth: '1024px' }}>
+                    <TextField
+                        className='search'
+                        fullWidth
+                        onChange={handleInputChange}
+                        label='Your name'
+                        placeholder='John Doe'
+                        color='secondary'
+                        sx={{
+                            '.MuiOutlinedInput-root': { backgroundColor: 'background.main' },
+                            '.MuiInputLabel-shrink': { color: 'secondary.main' },
+                            '&:is(*, :hover) .MuiOutlinedInput-notchedOutline': { borderColor: 'secondary.main' },
+                        }}
+                    />
+                    <Stack
+                        sx={{ my: '1em', flexWrap: 'wrap' }}
+                        direction='row'
+                        justifyContent='space-evenly'
+                        alignItems='flex-start'
+                    >
+                        {data.map((d) => (
+                            <Card sx={{ minWidth: '15em', backgroundColor: 'primary.accent', m: '5px' }}>
+                                <CardContent>
+                                    <Typography variant='h5' gutterBottom component='h5'>
+                                        {d.names[0]?.name ?? 'Unknown'}
+                                    </Typography>
+                                    <Typography
+                                        sx={{ fontSize: '0.75em', mb: '0.5em', mt: '-0.5em' }}
+                                        variant='p'
+                                        component='p'
+                                    >
+                                        Found {d.names[0]?.articles.length ?? 0} times
+                                    </Typography>
+                                    <Typography variant='p' gutterBottom component='p' align='left'>
+                                        Alternatives include:
+                                    </Typography>
+                                    {d.names?.filter((n) => n.freq > 1).length < 2 ? (
+                                        <Typography
+                                            sx={{ fontSize: '0.8em' }}
+                                            variant='p'
+                                            gutterBottom
+                                            component='p'
+                                            align='left'
+                                        >
+                                            None
+                                        </Typography>
+                                    ) : (
+                                        d.names
+                                            .filter((n, i) => i !== 0 && n.freq > 1)
+                                            .slice(0, 3)
+                                            .map((n) => (
+                                                <Typography
+                                                    sx={{ fontSize: '0.8em' }}
+                                                    variant='p'
+                                                    gutterBottom
+                                                    component='p'
+                                                    align='left'
+                                                >
+                                                    {n.name} found {n.articles.length} times
+                                                </Typography>
+                                            ))
+                                    )}
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </Stack>
+                    <Paper className='articles' sx={{ border: '1px solid white' }}>
+                        <Toolbar
+                            sx={{
+                                justifyContent: 'left',
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                py: '5px',
+                                gap: '1em',
+                            }}
+                        >
+                            <Box className='row' sx={{ display: 'flex', flexWrap: 'wrap', gap: '1em' }}>
+                                <Typography variant='h6' component='div' sx={{ minWidth: '260px' }}>
+                                    Show Wikipedia articles with
+                                </Typography>
+                                <FormControl sx={{ minWidth: '100px' }} variant='standard'>
+                                    <Select
+                                        label='Name'
+                                        onChange={(event) =>
+                                            handleSelectedChange({ name: event.target.value, spelling: 0 })
+                                        }
+                                        value={selected.name}
+                                    >
+                                        {data.map((d, i) => (
+                                            <MenuItem value={i}>{d?.enName}</MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                            <Box className='row' sx={{ display: 'flex', flexWrap: 'wrap', gap: '1em' }}>
+                                <Typography variant='h6' component='div' sx={{ minWidth: '90px' }}>
+                                    written as
+                                </Typography>
+                                <FormControl sx={{ minWidth: '100px' }} variant='standard'>
+                                    <Select
+                                        label='Name'
+                                        onChange={(event) => handleSelectedChange({ spelling: event.target.value })}
+                                        value={selected.spelling}
+                                    >
+                                        {data[selected.name]?.names
+                                            .filter((n) => n.freq > 1)
+                                            .slice(0, 4)
+                                            .map((n, i) => (
+                                                <MenuItem value={i}>{n.name}</MenuItem>
+                                            ))}
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                        </Toolbar>
+                        <Box
+                            sx={{
+                                '.MuiDataGrid-root': {
+                                    border: 'none',
+                                    '& .MuiDataGrid-columnHeaders': { borderTop: 1 },
+                                    '& :is(.MuiDataGrid-cell, .MuiDataGrid-columnHeaders)': {
+                                        borderColor: 'primary.main',
+                                    },
+                                    '& .MuiDataGrid-iconSeparator': { fill: '#666' },
+                                    '& :is(.MuiDataGrid-columnHeaders, .MuiDataGrid-row:nth-child(2n-2))': {
+                                        background: '#888',
+                                    },
+                                },
+                            }}
+                        >
+                            <DataGrid rows={rows} columns={columns} pageSize={10} rowHeight={38} autoHeight />
+                        </Box>
+                    </Paper>
+                </Container>
+            </Container>
+        </ThemeProvider>
     )
 }
 
--- nameinjapanese/src/yourname.js
+++ nameinjapanese/src/yourname.js
@@ -1,52 +1,60 @@
-async function nameInJapanese(name) {
-    const articles = await searchForName(name)
-    const titles = articles.map((a) => a.title)
-    const jaTitles = await getAvailableLanguages(titles).then(getJapaneseTitles)
-    const names = extractNames(jaTitles)
-    const nameFreq = countFrequency(names.map((n) => n[1]))
-    const [spelling, freq] = Object.entries(nameFreq).reduce((max, curr) => (curr[1] > max[1] ? curr : max), [0, 0])
-
-    const alsoCommon = Object.entries(nameFreq).filter(
-        ([key, value]) => key !== spelling && value > freq * 0.5 && value > 1,
-    )
-
-    console.log(
-        `\nThe name "${name}" was found in the title of ${articles.length} articles on Wikipedia`,
-        `\n${jaTitles.length} of which were available in Japanese.`,
-    )
-    if (freq === 1) {
-        return console.log(`But none were mentioned more than once`)
+// Get Japanese Names
+// Description: Split full name into individual names then search Wikipedia for how they are spelled
+// Input:       [string] One or more names separated by whitespace (ex: John Doe)
+// Output:      [list] Objects containing #english articles, #japanese articles, and the names found
+async function getJapaneseNames(fullName) {
+    const names = fullName.split(/\s/)
+    const japaneseNames = []
+    for (let i = 0; i < names.length; i++) {
+        japaneseNames.push(await getJapaneseName(names[i]))
     }
-    console.log(`\n\nThe most common Japanese spelling is: ${spelling} with ${freq} occurances`)
-    if (alsoCommon.length) {
-        console.log('Alternatives include:')
-        for (let i = 0; i < alsoCommon.length; i++) {
-            console.log(`\t${alsoCommon[i][0]} found ${alsoCommon[i][1]} times`)
-        }
-    }
-
-    if (jaTitles.length > 0 && jaTitles.length <= 10) {
-        console.log(`There were not many results, but I found these articles which may be of help`)
-        jaTitles.forEach((title) => {
-            const article = articles.find((a) => a.pageid === title[0])
-            console.log(title[0], title[1], article.title, article.snippet)
-        })
-    }
-    return spelling
+    return japaneseNames
 }
 
-function countFrequency(arr) {
-    const freqs = {}
-    arr.forEach((v) => (freqs[v] = (freqs[v] ?? 0) + 1))
-    return freqs
+// Get Japanese Name
+// Description: Search Wikipedia for how the name is spelled in Japanese
+// Input:       [string] A name (ex: John)
+// Output:      [list] Object containing the name in Japanese, its frequency, total articles
+//              and a list of objects containing the titles and a snippet of articles
+async function getJapaneseName(name) {
+    const articlesWithName = await searchWikipediaForName(name)
+    const articlesWithJapaneseTitles = await getJapaneseTitles(articlesWithName)
+    const japaneseNames = extractJapaneseNames(name, articlesWithJapaneseTitles) // [{name, article{ja, en, snippet}}]
+    const japaneseNamesFreq = getNameFrequency(japaneseNames) // [{name, freq, inArticles[{ja, en, snippet}]}]
+
+    return {
+        enCount: articlesWithName.length,
+        jaCount: Object.keys(articlesWithJapaneseTitles).length,
+        names: japaneseNamesFreq,
+        enName: name,
+    }
 }
 
-function extractNames(pages) {
-    const names = []
-    pages.forEach((page) => page[1].split('・').forEach((name) => names.push([page[0], name])))
-    return names
+// Search Wikipedia For Name
+// Description: Search Wikipedia for articles with the name in the title
+// Input:       [string] A name (ex: John)
+// Output:      [list] A list of articles. Raw data from Wikipedia
+async function searchWikipediaForName(name) {
+    const articles = []
+    // Fetch the first 500 synchronously to find out how many results there are,
+    const first500 = await fetchArticlesFromWikipedia(name, 0)
+    const totalArticles = first500.query.searchinfo.totalhits
+    // then get the rest asynchronously
+    const fetches = []
+    for (let offset = 500; offset < totalArticles && offset < 10000; offset += 500) {
+        fetches.push(fetchArticlesFromWikipedia(name, offset))
+    }
+    const results = await Promise.all(fetches)
+    results.concat([first500]).forEach((p) => articles.push(...p.query.search))
+    return articles
 }
-function fetchArticles(name, offset) {
+
+// Fetch articles From Wikipedia
+// Description: Get up to 500 articles from Wikipedia with the name in the title
+// Input:       [string] A name (ex: John)
+//              [integer] Offset
+// Output:      [list] A list of articles. Raw data from Wikipedia
+function fetchArticlesFromWikipedia(name, offset) {
     return searchWikipedia({
         list: 'search',
         srlimit: 500,
@@ -55,49 +63,103 @@ function fetchArticles(name, offset) {
     })
 }
 
-async function searchForName(name) {
-    const articles = []
-    const first500 = await fetchArticles(name, 0)
-    const promises = []
-    for (let i = 500; i < first500.query.searchinfo.totalhits; i += 500) {
-        promises.push(fetchArticles(name, i))
-    }
-    const results = await Promise.all(promises)
-    results.concat([first500]).forEach((p) => articles.push(...p.query.search))
-    return articles
-}
-function fetchLanguages(titles) {
-    return searchWikipedia({
-        prop: 'langlinks',
-        lllimit: 500,
-        titles,
-    })
-}
-
-async function getAvailableLanguages(titles) {
-    const pages = []
-    const promises = []
-    for (let i = 0; i < titles.length; i += 50) {
-        promises.push(fetchLanguages(titles.slice(i, i + 50).join('|')))
-    }
-    const results = await Promise.all(promises)
-    results.forEach((p) => pages.push(...Object.values(p.query.pages)))
-    return pages
-}
-
-function getJapaneseTitles(pages) {
-    return Object.values(pages)
-        .map((page) => [page.pageid, page.langlinks?.find((link) => link.lang === 'ja')?.['*']]) // Extract Japanese title
-        .filter((page) => page[1]) // Filter out pages without Japanese titles
-}
-
-function searchWikipedia(searchParams) {
+// Search Wikipedia
+// Description: Queries the Wikipedia API
+// Input:       [object] Search parameters
+// Output:      [object] Wikipedia response
+async function searchWikipedia(searchParams) {
     const config = { action: 'query', format: 'json', namespace: 0, ...searchParams }
     const query = Object.entries(config)
         .map((c) => c.join('='))
         .join('&')
     const url = `https://en.wikipedia.org/w/api.php?origin=*&${query}`
-    return fetch(url).then((r) => r.json())
+    return await fetch(url).then((r) => r.json())
 }
 
-export default nameInJapanese
+// Get Japanese Titles
+// Description: Search gets the Japanese titles of the wikipedia articles
+// Input:       [string] A name (ex: John)
+// Output:      [object] {pageid: {id, en, ja, snippet}, length}
+async function getJapaneseTitles(articles) {
+    const pageids = articles.map((a) => a.pageid)
+    // Get Japanese titles
+    const promises = []
+    for (let i = 0; i < pageids.length; i += 50) {
+        promises.push(fetchLanguagesFromWikipedia(pageids.slice(i, i + 50).join('|'), 'ja'))
+    }
+    const results = await Promise.all(promises)
+    const pages = []
+    results.forEach((p) => pages.push(...Object.values(p.query.pages)))
+    // Get articles that have Japanese titles
+    const japaneseArticles = {}
+    pages.forEach((page) => {
+        if (page.langlinks) japaneseArticles[page.pageid] = { id: page.pageid, ja: page.langlinks[0]['*'] }
+    })
+    // Add English title and snippet
+    articles.forEach((article) => {
+        const page = japaneseArticles[article.pageid]
+        if (page) {
+            page.en = article.title
+            page.snippet = new DOMParser().parseFromString(article.snippet, 'text/html').body.textContent ?? ''
+        }
+    })
+    return japaneseArticles
+}
+
+// Fetch Languages From Wikipedia
+// Description: Gets alternative languages of list of articles
+// Input:       [string] A list of tittles separated by "|"
+//              [string] Language code (ex: ja)
+// Output:      [Object] Response from Wikipedia
+function fetchLanguagesFromWikipedia(pageids, langCodes) {
+    return searchWikipedia({
+        prop: 'langlinks',
+        lllimit: 500,
+        lllang: langCodes,
+        pageids,
+    })
+}
+
+// Extract Japanese Names
+// Description: Extracts the japanese names and creates a list
+// Input:       [object] Wikipedia articles
+// Output:      [array] Objects with name and articles
+function extractJapaneseNames(name, articles) {
+    name = name.toLowerCase()
+    const names = []
+    Object.values(articles).forEach((article) => {
+        const enNameIndex = article.en.toLowerCase().split(/\s+/).indexOf(name)
+        const jaName = article.ja.split('・')[enNameIndex]
+        if (jaName) names.push({ name: jaName, article })
+    })
+    return names
+}
+
+// Get Name Frequency
+// Description: Finds out the frequency of each name
+// Input:       [{name, article}]
+// Output:      [{freq, name, articles}]
+function getNameFrequency(data) {
+    const names = data.map((e) => e.name)
+    const freqs = getFrequency(names)
+    const articlesMap = {}
+    data.forEach((d) => {
+        if (!articlesMap[d.name]) articlesMap[d.name] = []
+        articlesMap[d.name].push(d.article)
+    })
+    const freqList = Object.entries(freqs).map(([name, freq]) => ({ name, freq, articles: articlesMap[name] }))
+    freqList.sort((a, b) => b.freq - a.freq)
+    return freqList // [{freq, name, articles}]
+}
+
+// Get Frequency
+// Description: Calculates frequency of items in array
+// Input:       [item1, item2, ...]
+// Output:      {item1: freq, ...}
+function getFrequency(arr) {
+    const freqs = {}
+    arr.forEach((v) => (freqs[v] = (freqs[v] ?? 0) + 1))
+    return freqs
+}
+
+export default getJapaneseNames
